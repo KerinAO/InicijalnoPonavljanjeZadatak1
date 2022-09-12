a70244140df9ce1849e2cc01e2b9fc4613e5af51
KerinAO <90246117+KerinAO@users.noreply.github.com>
2022-09-12 11:58:13 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InicijalnoPonavljanje
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 100.00;
+            double b = 3.00;
+            double y = Math.Truncate(a/b);
+            double x = (a / b) - y;
+
+
+            Console.WriteLine(a + " dijeljeno s " + b + " je " + y + " i ostatak " + x);
+
+            Console.ReadKey();
+        }
+    }
+}
